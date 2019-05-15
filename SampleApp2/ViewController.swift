6bb5cd38b0b666aedb41648c06f18e743c35b7e9
Hariom <hariom.hamaraj.jss@gmail.com>
2019-05-15 21:18:09 +0530

    Some changes in ht-123456

--- SampleApp2/ViewController.swift
+++ SampleApp2/ViewController.swift
@@ -14,8 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //This is in master branch
+        //This is for ht-123456 branch
     }
 
 
 }
+
 
